81903b98de49be0176302f0253e5dc093dd0a284
v-kabashnikov <vania.kabashnikov@gmail.com>
2017-09-13 18:12:43 +0300

    creds changed

--- lib/bot_command/base.rb
+++ lib/bot_command/base.rb
@@ -11,13 +11,13 @@ module BotCommand
       token = ENV['token']
       @api = ::Telegram::Bot::Api.new(token)
       @greeting = "Привет, меня зовут Мозгва_бот.\nЯ могу показать тебе актуальное расписание на ближайшие игры.\nИли даже зарегистрировать твою команду.\nВот что я умею:\n/game_registration - Регистриция на игру \n/schedule - Расписание игр\n/settings - Персональные настройки (Имя, название команды, секретный код, номер телефона. Используются при регистрации)"
-      # @mozgva_url = "https://mozgva.com"
-      # @mozgva_api_key = "Test_afisha_api_key_654321"
-      # @id = "1"
+      @mozgva_url = "https://mozgva.com"
+      @mozgva_api_key = "Test_afisha_api_key_654321"
+      @id = "1"
 
-      @mozgva_url = "https://mozgva-staging.herokuapp.com"
-      @mozgva_api_key = "test_bot_api_key"
-      @id = "11"
+      # @mozgva_url = "https://mozgva-staging.herokuapp.com"
+      # @mozgva_api_key = "test_bot_api_key"
+      # @id = "11"
     end
 
     def should_start?
